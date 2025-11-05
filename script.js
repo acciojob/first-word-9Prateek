de7a1317a7a14af2047460cc7368dfd0159c5ac7
prateek <pratikkumar09835@gmail.com>
2025-11-05 21:26:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  str = str.trimStart();   // remove leading spaces
+  if (str === "") return "";
+  const idx = str.indexOf(" ");
+  return idx === -1 ? str : str.substring(0, idx);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
